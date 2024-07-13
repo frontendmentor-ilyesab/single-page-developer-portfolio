8eb5cb6f4827e46c04a90f24e85e411ddf92da94
ilyesab <abouelhoudailiass1@gmail.com>
2024-07-13 21:26:27 +0100

    switch to relative import

--- api/server.ts
+++ api/server.ts
@@ -1,36 +1,5 @@
-import express, {Router} from "express";
+import api from "../netlify/functions/api.ts";
 import ViteExpress from "vite-express";
-
-const API_URL = process.env.GOOGLE_SHEETS_API_URL ?? "";
-const API_ENDPOINT = process.env.API_ENDPOINT ?? ""
-
-const api = express();
-
-const router = Router();
-
-api.use(express.json());
-
-router.post(API_ENDPOINT, async (req, res) => {
-    const requestData = req.body;
-    const data = {
-      id: "INCREMENT",
-      date: "DATETIME",
-      ...requestData,
-    };
-    const response = await fetch(API_URL, {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        data: data,
-      }),
-    });
-    res.status(response.status).json(await response.json());
-  });
-
-api.use("/api/", router);
 
 ViteExpress.listen(api, 3000, () => console.log("Server is listening..."));
 
--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,5 +1,37 @@
+import express, {Router} from "express";
 import serverless from "serverless-http";
-import api from "../../api/server.ts";
+
+const API_URL = process.env.GOOGLE_SHEETS_API_URL ?? "";
+const API_ENDPOINT = process.env.API_ENDPOINT ?? ""
+
+const api = express();
+
+const router = Router();
+
+api.use(express.json());
+
+router.post(API_ENDPOINT, async (req, res) => {
+    const requestData = req.body;
+    const data = {
+      id: "INCREMENT",
+      date: "DATETIME",
+      ...requestData,
+    };
+    const response = await fetch(API_URL, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        data: data,
+      }),
+    });
+    res.status(response.status).json(await response.json());
+  });
+
+api.use("/api/", router);
 
 
 export const handler = serverless(api);
+export default api;
